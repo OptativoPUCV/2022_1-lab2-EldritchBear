3afc2f9c7d2c01c43f30e7b75a517a453ea7f872
EldritchBear <>
2022-03-19 19:55:54 +0000

    new try

--- list.c
+++ list.c
@@ -46,6 +46,7 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
+  if(list->current == NULL)return NULL;
   if(list->current->next != NULL)
     list->current = list->current->next;
   else
