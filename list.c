3d03cb983b6faa9062abaa395ce0347393c5be3c
EldritchBear <>
2022-03-19 19:59:27 +0000

    new try

--- list.c
+++ list.c
@@ -55,6 +55,7 @@ void * nextList(List * list) {
 }
 
 void * lastList(List * list) {
+  if(list->current == NULL)return NULL;
   list->current = list->tail;
   return list->current->data;
 }
@@ -63,7 +64,7 @@ void * prevList(List * list) {
   if(list->current == NULL)return NULL;
   if (list->current->prev != NULL)
     list->current = list->current->prev;
-  return list->current;
+  return list->current->data;
 }
 
 void pushFront(List * list, void * data) {
